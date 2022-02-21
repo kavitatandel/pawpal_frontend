966eab069a7b2b51560c551b17328534bebe2f53
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2022-02-21 12:37:15 +0100

    Google Map bug resolved

--- src/components/Forms/SearchForm.js
+++ src/components/Forms/SearchForm.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import bgImage from "../../assets/images/backgrounds/giorgia-finazzi-p73awrEBovI-unsplash-cropped.jpeg";
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
@@ -9,150 +9,168 @@ import MKButton from "../MKButton";
 // @mui material components
 import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
+// NEW GOOGLE MAP API
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
-//for map
-import Map from "components/Maps/Map";
-import {
-    // existing imports
-    withGoogleMap,
-    withScriptjs,
-} from "react-google-maps";
+// NEW GOOGLE MAP API
 
-<Map />;
-
-const MapWrapped = withScriptjs(withGoogleMap(Map));
+const mapContainerStyle = {
+  width: "100vw",
+  height: "85vh",
+  zIndex: -2,
+  border: "5px solid red",
+};
+const center = {
+  lat: 51.0647,
+  lng: 12.0128,
+};
 
 const SearchForm = () => {
-    return (
-        <>
-            {/* Container between top & Footer */}
-            <MKBox
-                px={1}
-                width="100%"
-                top={0}
-                height="85vh"
-                mx="auto"
-                mr={0}
-                ml={0}
-                position="relative"
-                zIndex={-1}
-                sx={{ padding: "0" }}
-                display="flex"
-                flexDirection="column"
-                justifyContent="flex-start"
-                alignItems="center"
-            >
-                {/* Container for top background Image */}
-                <MKBox
-                    minHeight="20vh"
-                    width="100%"
-                    //   style={{ border: "3px solid green" }}
-                    sx={{
-                        backgroundImage: ({
-                            functions: { linearGradient, rgba },
-                            palette: { gradients },
-                        }) =>
-                            `${linearGradient(
-                                rgba(gradients.dark.main, 0.2),
-                                rgba(gradients.dark.state, 0.2)
-                            )}, url(${bgImage})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        display: "grid",
-                        placeItems: "center",
-                    }}
-                />
-                {/* Container for body area below featured img */}
-                <MKBox
-                    display="flex"
-                    flexDirection="column"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    minHeight="80vh"
-                    top={0}
-                    width="100%"
-                //   style={{ border: "3px solid red" }}
-                //   style={{ border: "3px solid green" }}
-                >
-                    {/* Container for body area below featured img */}
-                    <MKBox
-                        display="flex"
-                        flexDirection="column"
-                        justifyContent="flex-start"
-                        alignItems="center"
-                        minHeight="80vh"
-                        top={0}
-                        width="100%"
-                    //   style={{ border: "3px solid red" }}
-                    //   style={{ border: "3px solid green" }}
-                    >
-                        <Card
-                            // zIndex={0}
-                            style={{ position: "relative" }}
-                            sx={{
-                                width: "90%",
-                                height: "80%",
-                                p: 2,
-                                mt: -2,
-                                mx: { xs: 2, lg: 3 },
-                                position: "relative",
-                                mb: 4,
-                                backgroundColor: ({ palette: { white }, functions: { rgba } }) =>
-                                    rgba(white.main, 0.8),
-                                backdropFilter: "saturate(200%) blur(30px)",
-                                boxShadow: ({ boxShadows: { xxl } }) => xxl,
-                            }}
-                        >
-                            <Container>
-                                <MKBox pt={4} pb={3} px={3} float="left" >
-                                    <MKTypography
-                                        variant="h3"
-                                        fontWeight="large"
-                                        color="dark"
-                                        textAlign="center"
-                                    >
-                                        {/* {`${user.first_name} ${user.last_name}`} */}
-                                        User Name
-                                    </MKTypography>
-                                    <MKInput
-                                        label="Search by City"
-                                        fullWidth
-                                        type="text"
-                                        name="searchCity"
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAQpCCsxnsWyctLl_hcwnW0mv1g9TrgVS8",
+  });
+  const [map, setMap] = useState(null);
 
-                                    />
-                                </MKBox>
-                                {/* Box to show searched Dogs */}
-                                <MKBox pt={4} pb={3} px={3} float="left" >
-                                    <MKTypography>
-                                        Searched Dogs Goes Here
-                                    </MKTypography>
-                                </MKBox>
-                                {/* Box to display map */}
-                                <MKBox pt={4} pb={3} px={3} >
-                                    <MKTypography>
-                                        Map goes here
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
 
-                                    </MKTypography>
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
-                                    <div style={{ width: "100vw", height: "100vh" }}>
-                                        <MapWrapped
-                                            //googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-                                            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAQpCCsxnsWyctLl_hcwnW0mv1g9TrgVS8`}
-                                            loadingElement={<div style={{ height: `100%` }} />}
-                                            containerElement={<div style={{ height: `100%` }} />}
-                                            mapElement={<div style={{ height: `100%` }} />}
-                                        />
-                                    </div>
+  return (
+    <>
+      <div
+        className="masterContainer"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* // Map Container */}
 
-                                </MKBox>
-                            </Container>
-                        </Card>
-                    </MKBox>
-                </MKBox>
-            </MKBox>
-        </>
-    )
-}
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center={center}
+            zoom={7}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {/* Child components, such as markers, info windows, etc. */}
+            <>
+              <Marker position={center}></Marker>
+            </>
+          </GoogleMap>
+        ) : (
+          <>
+            <h1>There is a problem</h1>
+          </>
+        )}
+        {/* <MKBox
+          display="flex"
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="center"
+          minHeight="85vh"
+          top={0}
+          width="100%" */}
+        {/* //   style={{ border: "3px solid red" }}
+          //   style={{ border: "3px solid green" }}
+        > */}
+        {/* Container for body area below featured img */}
+        {/* <MKBox
+            display="flex"
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            minHeight="80vh"
+            top={0}
+            width="100%" */}
+        {/* //   style={{ border: "3px solid red" }}
+            //   style={{ border: "3px solid green" }}
+          > */}
+        {/* <Card
+              // zIndex={0}
+              style={{ position: "relative" }}
+              sx={{
+                width: "90%",
+                height: "80%",
+                p: 2,
+                mt: -2,
+                mx: { xs: 2, lg: 3 },
+                position: "relative",
+                mb: 4,
+                backgroundColor: ({
+                  palette: { white },
+                  functions: { rgba },
+                }) => rgba(white.main, 0.8),
+                backdropFilter: "saturate(200%) blur(30px)",
+                boxShadow: ({ boxShadows: { xxl } }) => xxl,
+              }}
+            > */}
+
+        {/* Container for overlay over map */}
+        <MKBox
+          px={1}
+          width="80%"
+          top={100}
+          height="auto"
+          mx="auto"
+          mr={0}
+          ml={0}
+          position="fixed"
+          zIndex={-1}
+          sx={{ padding: "0" }}
+          display="flex"
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="center"
+          style={{
+            border: "3px solid blue",
+            backgroundColor: "rgba(39, 46, 245, 0.2)",
+          }}
+        >
+          <MKBox pt={4} pb={3} px={3} float="left">
+            <MKTypography>Searched Dogs Goes Here</MKTypography>
+          </MKBox>
+          <MKTypography
+            variant="h3"
+            fontWeight="large"
+            color="dark"
+            textAlign="center"
+          >
+            {/* {`${user.first_name} ${user.last_name}`} */}
+            User Name
+          </MKTypography>
+          <MKInput
+            label="Search by City"
+            type="text"
+            name="searchCity"
+            style={{ backgroundColor: "white" }}
+          />
+        </MKBox>
+        {/* Box to show searched Dogs */}
+
+        {/* <MKBox pt={4} pb={3} px={3}>
+            <MKTypography>Map goes here</MKTypography>
+            {/* <div style={{ width: "100vw", height: "100vh" }}> */}
+
+        {/* </div> */}
+        {/* </MKBox>  */}
+
+        {/* </Card> */}
+
+        {/* </MKBox> */}
+        {/* </MKBox> */}
+      </div>
+    </>
+  );
+};
 
 export default SearchForm;
--- src/components/Maps/Map.js
+++ src/components/Maps/Map.js
@@ -1,25 +1,38 @@
-import { GoogleMap, Marker } from "react-google-maps";
-import * as dogLocData from '../../data/GermanyData.json'
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+
+import * as dogLocData from "../../data/GermanyData.json";
 
 const Map = () => {
-    return (
-        <GoogleMap defaultZoom={10} defaultCenter={{ lat: 45.4211, lng: -75.6903 }}>
-            { /* We will render our data here */}
-            {dogLocData.map((dogLoc, index) => (
-                <Marker
-                    key={index}
-                    position={{
-                        lat: dogLoc.latitude,
-                        lng: dogLoc.longitude
-                    }}
-                    icon={{
-                        url: `https://picsum.photos/id/237/200/300`,
-                        scaledSize: new window.google.maps.Size(25, 25)
-                    }}
-                />
-            ))}
-        </GoogleMap>
-    );
-}
+  return (
+    // NEW REACT MAPS
+    <GoogleMap
+      onLoad={(Map) => {
+        const bounds = new window.google.maps.LatLngBounds();
+        Map.fitBounds(bounds);
+      }}
+      onUnmount={(Map) => {
+        // do your stuff before map is unmounted
+      }}
+    />
+
+    // OLD REACT MAPS
+    // <GoogleMap defaultZoom={10} defaultCenter={{ lat: 45.4211, lng: -75.6903 }}>
+    //   {/* We will render our data here */}
+    //   {dogLocData.map((dogLoc, index) => (
+    //     <Marker
+    //       key={index}
+    //       position={{
+    //         lat: dogLoc.latitude,
+    //         lng: dogLoc.longitude,
+    //       }}
+    //       icon={{
+    //         url: `https://picsum.photos/id/237/200/300`,
+    //         scaledSize: new window.google.maps.Size(25, 25),
+    //       }}
+    //     />
+    //   ))}
+    // </GoogleMap>
+  );
+};
 
 export default Map;
